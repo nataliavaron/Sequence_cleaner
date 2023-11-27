1f8d3008a2cf2d55be21b8a6ba283d7c507e6731
Angie Natalia Varón Saavedra <143977292+nataliavaron@users.noreply.github.com>
2023-11-27 22:10:04 +0100

    Seq_cleaner.py

--- Seq_cleaner.py
+++ Seq_cleaner.py
@@ -1,13 +1,13 @@
 import os
 import pandas as pd
 
-def procesar_archivo_phd(file_path, header_lines=5):
-    nucleotidos = []
-    calidades = []
+def process_phd_file(file_path, header_lines=5):
+    nucleotides = []
+    qualities = []
 
     with open(file_path, "r") as file:
         for i in range(header_lines):
-            next(file)  # Salta la línea actual
+            next(file)  # skip the current line
 
         in_final_section = False
 
@@ -16,59 +16,59 @@ def procesar_archivo_phd(file_path, header_lines=5):
             if line.startswith("END") or not line:
                 in_final_section = True
             if not in_final_section:
-                parts = line.split()  # Suponiendo que las secuencias y calidades están separadas por espacios
+                parts = line.split() 
                 if len(parts) >= 2:
-                    nucleotidos.append(parts[0])
-                    calidades.append(int(parts[1]))  # Convierte la calidad a un número entero
+                    nucleotides.append(parts[0])
+                    qualities.append(int(parts[1]))  
 
-    data = {'Nucleótidos': nucleotidos, 'Calidades': calidades}
+    data = {'Nucleótides': nucleotides, 'Qualities': qualities}
     df = pd.DataFrame(data)
     return df
 
-def procesar_carpeta(carpeta_path):
-    # Crear la carpeta "secuencias_curadas" si no existe
-    carpeta_curada = os.path.join(carpeta_path, "secuencias_curadas")
-    os.makedirs(carpeta_curada, exist_ok=True)
+def procesar_folder(folder_path):
+    # Create the "curated_sequence" folder if it doesn´t exist
+    curated_folder = os.path.join(folder_path, "curated_sequences")
+    os.makedirs(curated_folder, exist_ok=True)
 
-    for archivo in os.listdir(carpeta_path):
-        archivo_path = os.path.join(carpeta_path, archivo)
-        if os.path.isfile(archivo_path) and archivo.lower().endswith(".phd"):  # Filtrar por archivos .phd
-            df_temp = procesar_archivo_phd(archivo_path, header_lines=5)
+    for file in os.listdir(carpeta_path):
+        archivo_path = os.path.join(folder_path, file)
+        if os.path.isfile(file_path) and archivo.lower().endswith(".phd"): 
+            df_temp = process_phd_files(file_path, header_lines=5)
 
-            # Realizar operaciones adicionales según sea necesario con el DataFrame df_temp
+            # Perform additional operations as needed with the DataFrame df_temp
             # ...
 
-            # Puedes imprimir o hacer cualquier cosa con df_temp si lo necesitas
+            # print df_temp if needed
             print(df_temp)
 
-            # Filtrar y guardar el DataFrame en un nuevo archivo
-            valor_filtro = 30
-            nuevo_df = df_temp[df_temp['Calidades'] >= valor_filtro]
+            # filter and save the DataFrame to a new file
+            filter_value = 30
+            new_df = df_temp[df_temp['Qualities'] >= filter_value]
 
-            # Crear archivo FASTA
-            lista_nucleotidos = nuevo_df['Nucleótidos'].tolist()
+            # Create FASTA file
+            nucleotide_list = new_df['Nucleótides'].tolist()
 
-            # Nombre del archivo sin la extensión
-            nombre_sin_extension = os.path.splitext(archivo)[0]
+            # File name without extension
+            name_without_extension = os.path.splitext(file)[0]
 
             # Crear el nombre del archivo FASTA con el nombre del archivo original
-            fasta_file_name = f'nucleotidos_{nombre_sin_extension}.fasta'
-            fasta_file_path = os.path.join(carpeta_curada, fasta_file_name)
+            fasta_file_name = f'nucleotides_{name_without_extension}.fasta'
+            fasta_file_path = os.path.join(curated_folder, fasta_file_name)
 
-            # Guardar el archivo FASTA con el nombre del archivo original como identificador
+            # Save the FASTA file with the original file name as the identifier
             with open(fasta_file_path, 'w') as fasta_file:
-                fasta_file.write(f">{nombre_sin_extension}\n{''.join(lista_nucleotidos)}\n")
+                fasta_file.write(f">{name_without_extension}\n{''.join(nucleotide_list)}\n")
 
-# Llamar a la función y pasar la carpeta que contiene los archivos
+# Call the funtion and pass the folder containing the files
 if __name__ == "__main__":
     import sys
 
     if len(sys.argv) != 2:
-        print("Usage: python ensayo.py C:/Users/Diego Toro/Desktop/Archivos_clase/Proyecto final/Secuencias_sanger")
+        print("Usage: python archive.py folder address")
         sys.exit(1)
 
-    carpeta_con_archivos = sys.argv[1]
-    procesar_carpeta(carpeta_con_archivos)
+    folder_with_files = sys.argv[1]
+    process_folder(folder_with_files)
 
 
 
